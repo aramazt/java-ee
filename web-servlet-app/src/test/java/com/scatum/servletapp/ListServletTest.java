33c045ad2bb877e7371137debe79f36ac9a6721d
Aramazd <9408140+aramazdh@users.noreply.github.com>
2021-07-31 23:56:48 +0400

    removed senseless comments

--- web-servlet-app/src/test/java/com/scatum/servletapp/ListServletTest.java
+++ web-servlet-app/src/test/java/com/scatum/servletapp/ListServletTest.java
@@ -1,10 +1,3 @@
-/*
- * Copyrights 2015 FIP Software
- * Created: 12/2/18
- * Developer: Aramazd Hovhannisyan
- * This software is the proprietary information of FIP Software.
- * Its use is subject to License terms.
- */
 package com.scatum.servletapp;
 
 public class ListServletTest {
